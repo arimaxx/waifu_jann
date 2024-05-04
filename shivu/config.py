a875ff89af7dffba0189546277a54ce7b39f7e49
arimaxx <155280132+arimaxx@users.noreply.github.com>
2024-05-04 06:44:40 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,16 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
+    OWNER_ID = "6352061770"
+    sudo_users = "6352061770", "6176582997"
+    GROUP_ID = -1001939131996
+    TOKEN = "7159220855:AAF8SMfMDgI3IQU0i1vObY5-mCNu42v_CGY"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
+    SUPPORT_CHAT = "https://t.me/friendsgroupworld"
+    UPDATE_CHAT = "https://t.me/animetubeworld"
+    BOT_USERNAME = "gojosatoruriyabot"
+    CHARA_CHANNEL_ID = "-1001939131996"
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
